4b505fe457983656263f177c16cf589d5bcab83a
SergeyStepanovsky <98726536+SergeyStepanovsky@users.noreply.github.com>
2024-09-22 02:30:26 +0800

    updae

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+
+import com.example.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+
+}
