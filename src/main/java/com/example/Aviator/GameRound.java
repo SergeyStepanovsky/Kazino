82296945b42b74e30ccfc9d2dd6a929fb3a5c537
Aserim <Aserinia@mail.ru>
2024-09-22 20:03:36 +0800

    GameRound class added

--- src/main/java/com/example/Aviator/GameRound.java
+++ src/main/java/com/example/Aviator/GameRound.java
@@ -1,6 +1,39 @@
 package com.example.Aviator;
 
+import java.util.Random;
+
 public class GameRound {
     private double Multiplier;
     private double CrashPoint;
+    private Random random;
+    private final double ChanceToCrush = 0.05;
+
+    public GameRound() {
+        this.Multiplier = 1.0;
+        this.random = new Random();
+        this.CrashPoint = CreateCrashPoint();
+    }
+
+    private double CreateCrashPoint() {
+        double multiplier = 1.0;
+        while (true) {
+            if (random.nextDouble() <= ChanceToCrush) {
+                return multiplier;
+            }
+            multiplier += 0.1;
+        }
+    }
+
+    public double getCrashPoint() {
+        return CrashPoint;
+    }
+
+    public static void main(String[] args) {
+        GameRound round = new GameRound();
+        System.out.println("Улетел на " + round.getCrashPoint());
+    }
+
 }
+
+
+
---
+++ src/main/java/com/example/Aviator/Player.java
@@ -0,0 +1,12 @@
+package com.example.Aviator;
+
+public class Player {
+    private String Nickname;
+    private double Balance;
+
+    public Player(String Nickname, double StartingBalance) {
+        this.Nickname = Nickname;
+        this.Balance = StartingBalance;
+    }
+
+}
